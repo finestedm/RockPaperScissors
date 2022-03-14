4706f6184e2222e21b3de555f9b87584eff43161
pawel <finestedm@gmail.com>
2022-03-14 21:33:28 +0100

    working built

--- myscripts.js
+++ myscripts.js
@@ -6,29 +6,29 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'paper') {
         switch (computerSelection) {
             case 'paper':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'rock':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'scissors':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     } else if (playerSelection == 'rock') {
         switch (computerSelection) {
             case 'rock':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'scissors':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'paper':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     } else if (playerSelection == 'scissors') {
         switch (computerSelection) {
             case 'scissors':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'paper':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'rock':
-                return `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose!`;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     }
 
@@ -38,20 +38,20 @@ function playGame() {
     var playerSelection = prompt('Choose: Rock, Paper or Scissors?').toLowerCase();
     var computerChooser = Math.floor(Math.random() * computerPossibleChoices.length);
     var computerSelection = (computerPossibleChoices[computerChooser]);
-    return (playRound(playerSelection, computerSelection))
+    return (playRound(playerSelection, computerSelection));
 }
 
 for (let i = 0; i < 5; i++) {
-    console.log(playGame())
+    var returnedValues = (playGame());
+    console.log(returnedValues.firstNum);
+    score = + (returnedValues.secondNum);
 
 }
 
-/*
-case 'paper':
-    return `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`;
-case 'rock':
-    return `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
-    score = + 1;
-case 'scissors':
-    return `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose!`;
-*/
+if (score === 0) {
+    console.log(`After 5 rounds it's a draw!`);
+} else if (score > 0) {
+    console.log(`After 5 rounds you win with a score of ${score}!`);
+} else if (score < 0) {
+    console.log(`After 5 rounds you lose with a score of ${score}`);
+}
