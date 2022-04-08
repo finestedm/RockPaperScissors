3ceb842f875c27a7ab557191276675124ba299a0
pawel <finestedm@gmail.com>
2022-04-08 23:05:18 +0200

    working version with a bit of styling and additional features missing

--- myscripts.js
+++ myscripts.js
@@ -1,70 +1,110 @@
-
 var computerPossibleChoices = ['rock', 'paper', 'scissors'];
-var score = '';
+var count = 0;
+var computerScore = 0;
+var playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'paper') {
         switch (computerSelection) {
             case 'paper':
-                return 0;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'rock':
-                return 1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'scissors':
-                return -1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     } else if (playerSelection == 'rock') {
         switch (computerSelection) {
             case 'rock':
-                return 0;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'scissors':
-                return 1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'paper':
-                return -1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     } else if (playerSelection == 'scissors') {
         switch (computerSelection) {
             case 'scissors':
-                return 0;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
             case 'paper':
-                return 1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
             case 'rock':
-                return -1;
+                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
         }
     }
-
 }
 
 function playGame(playerChoise) {
     var playerSelection = playerChoise;
     var computerChooser = Math.floor(Math.random() * computerPossibleChoices.length);
     var computerSelection = (computerPossibleChoices[computerChooser]);
-    roundPoint = (playRound(playerSelection, computerSelection));
-    return roundPoint;
+    roundResult = (playRound(playerSelection, computerSelection));
+    calculatingResult(roundResult);
+}
+
+function calculatingResult(roundResult) {
+    roundPoint = (roundResult.secondNum);
+    if (roundPoint < 0) {
+        computerScore++;
+    } else if (roundPoint > 0) {
+        playerScore++;
+    }
+    count++;
+
+    scoreUpdater(roundResult.firstNum);
+
+    if (count === 5) {
+        if (playerScore > computerScore) {
+            var thisRoundScore = 'After 5 rounds you have won';
+        } else if (playerScore < computerScore) {
+            var thisRoundScore = 'After 5 rounds You have lost';
+        } else {
+            var thisRoundScore = 'After 5 rounds It`a tie';
+        }
+        document.getElementById("thisRound").innerHTML = thisRoundScore
+
+    }
 
 }
 
+function resetGame() {
+    count = 0;
+    playerScore = 0;
+    computerScore = 0;
+    scoreUpdater();
+}
+
+function scoreUpdater(textResult) {
+    document.getElementById("counter").innerHTML = count;
+    document.getElementById("computerScorer").innerHTML = computerScore;
+    document.getElementById("playerScorer").innerHTML = playerScore;
+    document.getElementById("thisRound").innerHTML = textResult;
+
+}
 
 const result = document.querySelector('#result');
+const counter = document.querySelector('#counter');
+const computerScorer = document.querySelector('#computerScorer');
+const playerScorer = document.querySelector('#playerScorer');
+const thisRound = document.querySelector('#thisRound');
 
 
 const btnRock = document.querySelector('#btnRock');
 btnRock.addEventListener('click', () => {
-    var point = (playGame('rock'));
-    score = + point;
-    console.log(score);
+    playGame('rock');
 });
 
 const btnPaper = document.querySelector('#btnPaper');
 btnPaper.addEventListener('click', () => {
-    console.log(playGame('paper'));
-
+    playGame('paper');
 });
 
 const btnScissors = document.querySelector('#btnScissors');
 btnScissors.addEventListener('click', () => {
-    console.log(playGame('scissors'));
-
+    playGame('scissors');
 });
 
-
-
+const btnReset = document.querySelector('#reset');
+btnReset.addEventListener('click', () => {
+    resetGame();
+});
