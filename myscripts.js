ae96647c42d8c36a73ee4a699460cf90fac0b75c
pawel <finestedm@gmail.com>
2022-04-09 15:44:23 +0200

    working, stylized build with a splash screen at the start

--- myscripts.js
+++ myscripts.js
@@ -7,29 +7,29 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'paper') {
         switch (computerSelection) {
             case 'paper':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
+                return 0;
             case 'rock':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
+                return 1;
             case 'scissors':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
+                return -1;
         }
     } else if (playerSelection == 'rock') {
         switch (computerSelection) {
             case 'rock':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
+                return 0;
             case 'scissors':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
+                return 1;
             case 'paper':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
+                return -1;
         }
     } else if (playerSelection == 'scissors') {
         switch (computerSelection) {
             case 'scissors':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. It's a draw!`, secondNum: 0 };
+                return 0;
             case 'paper':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Win!`, secondNum: 1 };
+                return 1;
             case 'rock':
-                return { firstNum: `You chose ${playerSelection}. Computer chose ${computerSelection}. You Lose!`, secondNum: -1 };
+                return -1;
         }
     }
 }
@@ -39,19 +39,25 @@ function playGame(playerChoise) {
     var computerChooser = Math.floor(Math.random() * computerPossibleChoices.length);
     var computerSelection = (computerPossibleChoices[computerChooser]);
     roundResult = (playRound(playerSelection, computerSelection));
-    calculatingResult(roundResult);
+    calculatingResult(roundResult, playerSelection, computerSelection);
 }
 
-function calculatingResult(roundResult) {
-    roundPoint = (roundResult.secondNum);
-    if (roundPoint < 0) {
+function calculatingResult(roundResult, playerSelection, computerSelection) {
+    if (roundResult < 0) {
         computerScore++;
-    } else if (roundPoint > 0) {
+        currentRoundScore = 'You lose';
+    } else if (roundResult > 0) {
         playerScore++;
+        currentRoundScore = 'You win';
+    } else {
+        currentRoundScore = "It's a tie";
     }
     count++;
 
-    scoreUpdater(roundResult.firstNum);
+
+    var currentRoundTextResult = `You chose ${playerSelection} and Computer chose ${computerSelection}. ${currentRoundScore}.`
+
+    scoreUpdater(currentRoundTextResult);
 
     if (count === 5) {
         if (playerScore > computerScore) {
@@ -74,11 +80,11 @@ function resetGame() {
     scoreUpdater();
 }
 
-function scoreUpdater(textResult) {
+function scoreUpdater(currentRoundTextResult) {
     document.getElementById("counter").innerHTML = count;
     document.getElementById("computerScorer").innerHTML = computerScore;
     document.getElementById("playerScorer").innerHTML = playerScore;
-    document.getElementById("thisRound").innerHTML = textResult;
+    document.getElementById("thisRound").innerHTML = currentRoundTextResult;
 
 }
 
@@ -108,3 +114,11 @@ const btnReset = document.querySelector('#reset');
 btnReset.addEventListener('click', () => {
     resetGame();
 });
+
+
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
+        splash.classList.add('display-none');
+    }, 2000);
+})
